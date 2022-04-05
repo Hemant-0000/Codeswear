9f0194d8abc91b16ac5e56d745570efecaa328c3
Hemant <hrsmart2150@gmail.com>
2022-04-05 16:05:18 +0530

    Created API's for adding, getting and updating the products

--- models/Order.js
+++ models/Order.js
@@ -10,5 +10,5 @@ const OrderSchema = new mongoose.Schema({
     amount: { type: Number, required: true },
     status: { type: String, default: 'Pending', required: true }
 }, { timestamps: true });
-
+mongoose.models = {}
 export default mongoose.model("Order", OrderSchema)
