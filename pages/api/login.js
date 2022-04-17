ea1c8b9a9e6f6e6945896b02fea84100e56f567e
Hemant <hrsmart2150@gmail.com>
2022-04-17 10:40:53 +0530

    Wrapping up our codeswear.com

--- pages/api/login.js
+++ pages/api/login.js
@@ -7,11 +7,11 @@ var jwt = require('jsonwebtoken');
 const handler = async (req, res) => {
     if (req.method == 'POST') {
         let user = await User.findOne({ "email": req.body.email })
-        const bytes  = CryptoJS.AES.decrypt(user.password, 'SECRET_KEY');
+        const bytes  = CryptoJS.AES.decrypt(user.password, process.env.AES_SECRET);
         let decryptedPass = bytes.toString(CryptoJS.enc.Utf8);
         if (user) {
             if (req.body.email == user.email && req.body.password == decryptedPass) {
-                var token = jwt.sign({ email: user.email, name: user.name }, 'JWT_SECRET', { expiresIn: "2d" });   
+                var token = jwt.sign({ email: user.email, name: user.name }, process.env.JWT_SECRET, { expiresIn: "2d" });   
                 res.status(200).json({success: true, token})
             } else {
                 res.status(200).json({ success: false, error: "Invalid Credentials" })
--- pages/checkout.js
+++ pages/checkout.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AiOutlinePlusCircle, AiOutlineMinusCircle } from 'react-icons/ai';
-import { BsFillBagCheckFill } from 'react-icons/bs'
 import Link from 'next/link'
 
 
 const Checkout = ({ cart, subTotal, addToCart, removeFromCart }) => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [address, setAddress] = useState('')
+  const [pincode, setPincode] = useState('')
+  const [city, setCity] = useState('')
+  const [state, setState] = useState('')
+  const [disabled, setDisabled] = useState(true)
+  const handleChange = (e) => {
+    if (e.target.name == 'name') {
+      setName(e.target.value)
+    }
+    else if (e.target.name == 'email') {
+      setEmail(e.target.value)
+    }
+    else if (e.target.name == 'phone') {
+      setPhone(e.target.value)
+    }
+    else if (e.target.name == 'address') {
+      setAddress(e.target.value)
+    }
+    else if (e.target.name == 'pincode') {
+      setPincode(e.target.value)
+    }
+    setTimeout(() => {
+      if (name.length > 3 && email.length > 3 && phone.length > 3 && address.length > 3 && pincode.length > 3) {
+        setDisabled(false)
+      } else {
+        setDisabled(true)
+      }
+    }, 100);
+  }
   return (
     <div className='container m-auto'>
       <h1 className='font-bold text-3xl my-8 text-center'>Checkout</h1>
@@ -13,13 +44,13 @@ const Checkout = ({ cart, subTotal, addToCart, removeFromCart }) => {
         <div className="px-2 w-1/2">
           <div className="mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
-            <input type="text" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <input onChange={handleChange} value={name} type="text" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
         </div>
         <div className="px-2 w-1/2">
           <div className="mb-4">
             <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
-            <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <input onChange={handleChange} value={email} type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
         </div>
       </div>
@@ -27,7 +58,7 @@ const Checkout = ({ cart, subTotal, addToCart, removeFromCart }) => {
       <div className="mx-32">
         <div className="px-2 w-full">
           <label htmlFor="address" className="leading-7 text-sm text-gray-600">Address</label>
-          <textarea id="address" name="address" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" cols="30" rows="2"></textarea>
+          <textarea onChange={handleChange} value={address} id="address" name="address" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" cols="30" rows="2"></textarea>
         </div>
       </div>
 
@@ -35,13 +66,13 @@ const Checkout = ({ cart, subTotal, addToCart, removeFromCart }) => {
         <div className="px-2 w-1/2">
           <div className="mb-4">
             <label htmlFor="phone" className="leading-7 text-sm text-gray-600">Phone</label>
-            <input type="phone" id="phone" name="phone" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <input onChange={handleChange} value={phone} type="phone" id="phone" name="phone" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
         </div>
         <div className="px-2 w-1/2">
           <div className="mb-4">
-            <label htmlFor="city" className="leading-7 text-sm text-gray-600">City</label>
-            <input type="text" id="city" name="city" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <label htmlFor="pincode" className="leading-7 text-sm text-gray-600">Pincode</label>
+            <input onChange={handleChange} value={pincode} type="number" id="pincode" name="pincode" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
         </div>
       </div>
@@ -49,13 +80,13 @@ const Checkout = ({ cart, subTotal, addToCart, removeFromCart }) => {
         <div className="px-2 w-1/2">
           <div className="mb-4">
             <label htmlFor="state" className="leading-7 text-sm text-gray-600">State</label>
-            <input type="phone" id="state" name="state" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <input type="phone" id="state" value={state} name="state" readOnly={true} className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
         </div>
         <div className="px-2 w-1/2">
           <div className="mb-4">
-            <label htmlFor="pincode" className="leading-7 text-sm text-gray-600">Pincode</label>
-            <input type="number" id="pincode" name="pincode" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <label htmlFor="city" className="leading-7 text-sm text-gray-600">City</label>
+            <input type="text" id="city" value={city} name="city" readOnly={true} className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
         </div>
       </div>
@@ -78,7 +109,7 @@ const Checkout = ({ cart, subTotal, addToCart, removeFromCart }) => {
         </div>
         <div className="mx-1 my-2">
           <Link passHref href={'/checkout'}>
-            <button className="flex mr-2 text-white bg-gray-500 border-0 py-2 px-4 focus:outline-none hover:bg-gray-400 rounded text-sm">Pay ₹ {subTotal}</button>
+            <button disabled={disabled} className="disabled:bg-gray-400 flex mr-2 text-white bg-gray-500 border-0 py-2 px-4 focus:outline-none hover:bg-gray-400 rounded text-sm">Pay ₹ {subTotal}</button>
           </Link>
         </div>
       </div>
